7ff1b7315796976ad79c96b339154d596a46fc95
Saul Pierotti <saul@ebi.ac.uk>
2024-09-20 23:34:54 +0100

    update

--- filter_bgen.sh
+++ filter_bgen.sh
@@ -2,15 +2,22 @@
 #
 # Author: Saul Pierotti, European Bioinformatics Institute
 #
-# Filter snd reorder samples in a UKB bgen file given a phenotype file from the
+# Filter and reorder samples in a UKB bgen file given a phenotype file from the
 # eids present in such file, so that they match. Output in pgen format. Applies
 # custom variant filters. It takes in input the basename of the bgen file to
-# operate on, without extension. The output is written with the same basename
-# Assumes plink2 is installed..
+# operate on, without extension, and the phenotype/covariate dataset with the
+# eid column to be used. The eid column is assumed to be the first column. An
+# header is assumed to be present and removed.
+# The output is written with the same basename. Assumes plink2 is installed.
+
+$EIDS = $(mktemp)
+cat $2 | cut -d, -f1 | tail -n +1 >$EIDS
 
 plink2 \
     --bgen $1.bgen ref-first \
     --sample $1.sample \
     --maf 0.01 \
+    --keep $EIDS \
+    --indiv-sort file $EIDS \
     --make-pgen \
     --out $1
