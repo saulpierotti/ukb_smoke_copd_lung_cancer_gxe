f87e4984b8bed48713275c626c43d6e8e21c413a
Saul Pierotti <saul@ebi.ac.uk>
2024-09-20 23:12:33 +0100

    plink script

---
+++ filter_bgen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Author: Saul Pierotti, European Bioinformatics Institute
+#
+# Filter snd reorder samples in a UKB bgen file given a phenotype file from the
+# eids present in such file, so that they match. Output in pgen format. Applies
+# custom variant filters. It takes in input the basename of the bgen file to
+# operate on, without extension. The output is written with the same basename
+# Assumes plink2 is installed..
+
+plink2 \
+    --bgen $1.bgen ref-first \
+    --sample $1.sample \
+    --maf 0.01 \
+    --make-pgen $1
