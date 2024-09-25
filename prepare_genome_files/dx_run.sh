ebde1f3f2d22c81a45a2f570953ea7f1687a6514
Saul Pierotti <saul@ebi.ac.uk>
2024-09-25 14:10:58 +0100

    dx run

--- prepare_genome_files/dx_run.sh
+++ prepare_genome_files/dx_run.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#
+# Run the ./filter_bgen.sh script on a batch of inputs (basenames).
+# A file with one input per row is expected as first argument.
+
 while read id; do
-  echo "$id"
-done < ./id_list.txt
+    dx run swiss-army-knife \
+        -icmd="wget https://raw.githubusercontent.com/saulpierotti/ukb_smoke_copd_lung_cancer_gxe/refs/heads/main/prepare_genome_files/filter_bgen.sh && chmod +x filter_bgen.sh && ./filter_bgen.sh ${id} smoke_lung_cancer_copd.csv" \
+        -iin="Bulk/Imputation/UKB\ imputation\ from\ genotype/${id}.bgen" \
+        -iin="Bulk/Imputation/UKB\ imputation\ from\ genotype/${id}.bgen.bgi" \
+        -iin="Bulk/Imputation/UKB\ imputation\ from\ genotype/${id}.sample" \
+        -iin="Bulk/Imputation/UKB\ imputation\ from\ genotype/${id}.mfi.txt" \
+        -iin="pheno_cov/smoke_lung_cancer_copd.csv" \
+done < $1
