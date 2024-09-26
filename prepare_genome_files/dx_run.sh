1b3db1613d3f91b78df4fbbf218a311f9eae3dcf
Saul Pierotti <saul@ebi.ac.uk>
2024-09-26 11:25:59 +0100

    bug

--- prepare_genome_files/dx_run.sh
+++ prepare_genome_files/dx_run.sh
@@ -7,7 +7,8 @@ echo "Using ${1}"
 while read id; do
     echo "Submitting ${id}"
     dx run swiss-army-knife \
-        -icmd="wget https://raw.githubusercontent.com/saulpierotti/ukb_smoke_copd_lung_cancer_gxe/refs/heads/main/prepare_genome_files/filter_bgen.sh && chmod +x filter_bgen.sh && ./filter_bgen.sh ${id} smoke_lung_cancer_sample_list.txt" \
+        -icmd="bash filter_bgen.sh ${id} smoke_lung_cancer_copd_sample_list.txt" \
+        -iin="https://raw.githubusercontent.com/saulpierotti/ukb_smoke_copd_lung_cancer_gxe/refs/heads/main/prepare_genome_files/filter_bgen.sh" \
         -iin="Bulk/Imputation/UKB\ imputation\ from\ genotype/${id}.bgen" \
         -iin="Bulk/Imputation/UKB\ imputation\ from\ genotype/${id}.bgen.bgi" \
         -iin="Bulk/Imputation/UKB\ imputation\ from\ genotype/${id}.sample" \
